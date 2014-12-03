17feb40a05585dcee4f6859412e0256925530aeb
Einsiedler <alexander@geocomply.net>
2014-12-03 17:26:29 +0200

    GCM-4229 Create a build machine for Mac OS X & Windows Solutions, fixed issue with SDKROOT for mac os x platform.

--- prepmac.sh
+++ prepmac.sh
@@ -6,6 +6,8 @@ source "${0%/*}/common.sh" "$@"
 
 ARCH=-DCMAKE_OSX_ARCHITECTURES="${CMAKE_OSX_ARCHITECTURES:-i386;x86_64}"
 
+export SDKROOT="/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.6.sdk"
+
 pushd "$BUILDDIR"
 cmake -G "$GEN" -DFB_PROJECTS_DIR="${PROJDIR}" ${ARCH} "$@" "${FB_ROOT}"
 if [ "$?" -ne 0 ]; then
